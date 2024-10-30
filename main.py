a78746a7127dddf681bccbf0749889d9372bd2b9
Prasad Nirmal <113760595+Prasad-root@users.noreply.github.com>
2024-10-30 16:55:37 +0530

    main progrmme sector add to main function

--- main.py
+++ main.py
@@ -10,12 +10,13 @@ gesture_Hand = Gesture()
 capture = cv2.VideoCapture(0)
 capture.set(3, width)  # Set width for window
 capture.set(4, height)  # Set height for window
-
-
 hand_controlling_finger_pair = [[8, 5], [12, 9], [16, 13], [20, 17], [4, 1]]   # Map points to each finger
 
+#==========================================================================================
+
 def sendmessages(data):
-    global sended_message   
+    """This function is dedicated to send message from python programme to nodemcu"""
+    global sended_message,frame
     d = data
     if d != sended_message:
         #print(f"SENDED MESSAGE : {sended_message}  |  DATA  {data}")
@@ -32,80 +33,85 @@ def put_text_on_screen(pic_frame,text,cordinates,RGB_color):
     #print Fan Speed on Window
     cv2.putText(pic_frame, f"{text}", cordinates, cv2.FONT_HERSHEY_SIMPLEX, 1, RGB_color, 3)
 
-while True:
-    status, frame = capture.read()
-    frame = cv2.flip(frame,1)  # Read capture 
-    gesture_Hand.handDetect(frame)  # Detect Hand
-    codinates = gesture_Hand.collectPointCodinates(False)
-    gesture_Hand.stackTip2Base(frame, codinates, hand_controlling_finger_pair)
+#===========================================================================================
+def main_func():
+    global ptime,ctime,frame
+    while True:
+        status, frame = capture.read()
+        frame = cv2.flip(frame,1)  # Read capture 
+        gesture_Hand.handDetect(frame)  # Detect Hand
+        codinates = gesture_Hand.collectPointCodinates(False)
+        gesture_Hand.stackTip2Base(frame, codinates, hand_controlling_finger_pair)
 
-    output = gesture_Hand.distance_calculate_2points(hand_controlling_finger_pair)
+        output = gesture_Hand.distance_calculate_2points(hand_controlling_finger_pair)
 
-    ctime = time.time()
-    fps = int(1 / (ctime - ptime))
-    ptime = ctime
+        ctime = time.time()
+        fps = int(1 / (ctime - ptime))
+        ptime = ctime
 
-    cv2.putText(frame, f"FPS : {str(fps)}", (100, 80), cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 255, 0), 3)
+        cv2.putText(frame, f"FPS : {str(fps)}", (100, 80), cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 255, 0), 3)
 
-    first_finger = output[0][0]
-    second_finger = output[1][0]
-    third_finger = output[2][0]
-    fourth_finger = output[3][0]
+        first_finger = output[0][0]
+        second_finger = output[1][0]
+        third_finger = output[2][0]
+        fourth_finger = output[3][0]
 
-    first_finger_status = False
-    second_finger_status = False
-    third_finger_status = False
-    fourth_finger_status = False
-
-
-    if first_finger > 140:
-        first_finger_status = True
-    if second_finger > 155:
-        second_finger_status = True
-    if third_finger > 165:
-        third_finger_status = True
-    if fourth_finger > 150:
-        fourth_finger_status = True
-
-    
-    cv2.circle(frame, (100, 150), 30, (255, 255, 0), 3)
-    cv2.circle(frame, (100, 230), 30, (0, 0, 255), 3)
-    cv2.circle(frame, (100, 310), 30, (255, 255, 255), 3)
-    cv2.circle(frame, (100, 390), 30, (0, 255, 0), 3)
+        first_finger_status = False
+        second_finger_status = False
+        third_finger_status = False
+        fourth_finger_status = False
 
 
-    # Capture which fingers are rised
-    if fourth_finger_status:
-        # Turn on the fan
-        cv2.circle(frame, (100, 150), 30, (255, 255, 0), -1)
-        put_text_on_screen(frame,text="4",cordinates=(92,156),RGB_color=(0, 0, 0))
-        print_fan_statue(frame,"ON")
-        sendmessages("4")
+        if first_finger > 140:
+            first_finger_status = True
+        if second_finger > 155:
+            second_finger_status = True
+        if third_finger > 165:
+            third_finger_status = True
+        if fourth_finger > 150:
+            fourth_finger_status = True
 
-    elif third_finger_status:
-        cv2.circle(frame, (100, 230), 30, (0, 0, 255), -1)
-        put_text_on_screen(frame,text="3",cordinates=(92,236),RGB_color=(0,0,0))
-        print_fan_statue(frame,"ON")
-        sendmessages("3")
-    elif second_finger_status:
-        cv2.circle(frame, (100, 310), 30, (255, 255, 255), -1)
-        put_text_on_screen(frame,text="2",cordinates=(92,316),RGB_color=(0,0,0))
-        print_fan_statue("ON")
-        sendmessages("2")
-    elif first_finger_status:
-        cv2.circle(frame, (100, 390), 30, (0, 255, 0), -1)
-        put_text_on_screen(frame,text="2",cordinates=(92,396),RGB_color=(0,0,0))
-        print_fan_statue(frame,"ON")
-        sendmessages("1")
-    else:
-        sendmessages("0")
-        print_fan_statue(frame,"OFF")
+        
+        cv2.circle(frame, (100, 150), 30, (255, 255, 0), 3)
+        cv2.circle(frame, (100, 230), 30, (0, 0, 255), 3)
+        cv2.circle(frame, (100, 310), 30, (255, 255, 255), 3)
+        cv2.circle(frame, (100, 390), 30, (0, 255, 0), 3)
 
-    cv2.imshow("Web Camera", frame)
 
-    if cv2.waitKey(1) & 0xFF == ord("q"):
-        break
+        # Capture which fingers are rised
+        if fourth_finger_status:
+            # Turn on the fan
+            cv2.circle(frame, (100, 150), 30, (255, 255, 0), -1)
+            put_text_on_screen(frame,text="4",cordinates=(92,156),RGB_color=(0, 0, 0))
+            print_fan_statue(frame,"ON")
+            sendmessages("4")
 
-cv2.destroyAllWindows()
+        elif third_finger_status:
+            cv2.circle(frame, (100, 230), 30, (0, 0, 255), -1)
+            put_text_on_screen(frame,text="3",cordinates=(92,236),RGB_color=(0,0,0))
+            print_fan_statue(frame,"ON")
+            sendmessages("3")
+        elif second_finger_status:
+            cv2.circle(frame, (100, 310), 30, (255, 255, 255), -1)
+            put_text_on_screen(frame,text="2",cordinates=(92,316),RGB_color=(0,0,0))
+            print_fan_statue("ON")
+            sendmessages("2")
+        elif first_finger_status:
+            cv2.circle(frame, (100, 390), 30, (0, 255, 0), -1)
+            put_text_on_screen(frame,text="2",cordinates=(92,396),RGB_color=(0,0,0))
+            print_fan_statue(frame,"ON")
+            sendmessages("1")
+        else:
+            sendmessages("0")
+            print_fan_statue(frame,"OFF")
+
+        cv2.imshow("Web Camera", frame)
+
+        if cv2.waitKey(1) & 0xFF == ord("q"):
+            break
+
+if __name__ == "__main__":
+    main_func()
+    cv2.destroyAllWindows()
 
 
